809c4f516577a7125ff1a68a00302bd192d29f80
nikhithajarabana <113314797+nikhithajarabana@users.noreply.github.com>
2022-12-05 00:32:38 -0500

    Add files via upload

---
+++ Nikhitha_Procedures.sql
@@ -0,0 +1,36 @@
+CREATE OR REPLACE PROCEDURE Add_Customer IS
+    Customer_num number(10);
+    name varchar2(20);
+BEGIN
+        Customer_num := 21;	
+        name := 'Lisa';	
+        INSERT INTO anouksha.customer (Customer_ID, Customer_Name) VALUES (Customer_num, name);
+       DBMS_OUTPUT.PUT_LINE ('New Customer inserted');
+END;
+/
+
+
+BEGIN
+Add_Customer();
+
+END;
+/
+grant execute on add_customer to anouksha,rithvik,sahithi;
+
+
+CREATE OR REPLACE PROCEDURE Add_Product IS
+    product_num number(10);
+    name varchar2(20);
+BEGIN
+        Product_num := 11;	
+        name := 'Pantene';	
+        INSERT INTO anouksha.product (product_ID, Product_Name) VALUES (product_num, name);
+       DBMS_OUTPUT.PUT_LINE ('New Product inserted');
+END;
+/
+BEGIN
+Add_Product();
+END;
+/
+grant execute on add_product to anouksha,rithvik,sahithi;
+
